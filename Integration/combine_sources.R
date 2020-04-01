3c09cc1d04a741292455222fe60e7103032907ec
Marek Ostaszewski <marek.ostaszewski@uni.lu>
2020-04-01 21:02:48 +0200

    Integration - first files

---
+++ Integration/combine_sources.R
@@ -0,0 +1,10 @@
+##################################################
+## Project: COVID-19 Disease Map
+## Script purpose: Combine maps from different sources 
+## Date: 01.04.2020
+## Author: Marek Ostaszewski
+##################################################
+
+library(httr)
+
+read.csv()
